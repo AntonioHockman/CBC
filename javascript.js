57e2fb82ce0948b55bf092f8976c9c958a0c00bf
AntonioHockman <150075660+AntonioHockman@users.noreply.github.com>
2025-07-05 17:18:50 -0500

    added background color to sections

--- javascript.js
+++ javascript.js
@@ -1,7 +1,1 @@
-console.log("Hello, World!");
-// Add an onClick handler to elements with class 'ggDisc'
-document.querySelectorAll('.ggDisc').forEach(function(el) {
-	el.onclick = function() {
-		alert('Hello, World 2!');
-	};
-});
+
